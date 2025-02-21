c89c20e183dafc0be1fd8158a49f9c05e6cbce6c
bennyd6 <228r1a66d6@cmrec.ac.in>
2025-02-21 13:15:36 +0530

    started frontend user dashboard

---
+++ User/frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from './components/header'
+import Navbar from './components/navbar'
+import Home from './pages/home'
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Navbar></Navbar>
+      <Home></Home>
+    </>
+  )
+}
+
+export default App
---
+++ User/frontend/src/components/navbar.jsx
@@ -0,0 +1,16 @@
+import './navbar.css'
+
+export default function Navbar(){
+    return(
+        <>
+        <div className="nav-main">
+            {/* <div className="nav-in"> */}
+                <a href="">Home</a>
+                <a href="">Events</a>
+                <a href="">Chat</a>
+                <a href="">Settings</a>
+            {/* </div> */}
+        </div>
+        </>
+    )
+}
